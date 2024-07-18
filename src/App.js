8167196f054695d01a3934238084ec4c87e42dbd
HossamAbdelRahman177 <hosoff2@gmail.com>
2024-07-18 23:28:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
